b8a20aa2b905653628577143fdbc3e6279ac6f05
GiuseppeFalliti <141495925+GiuseppeFalliti@users.noreply.github.com>
2024-05-03 12:43:41 +0200

    Create Auto.java

---
+++ Auto.java
@@ -0,0 +1,14 @@
+public class Auto {
+    private String targa;
+
+   public  Auto(String targa) {
+        this.targa = targa;
+    }
+    public String getTarga() {
+        return targa;
+    }
+    public void setTarga(String targa) {
+        this.targa = targa;
+    }
+    
+}
